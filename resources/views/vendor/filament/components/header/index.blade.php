09298da318bba6249fc35f69322aed28aae528cc
Fred452 <fredyarce452@gmail.com>
2023-03-18 14:53:39 -0300

    Agregando logo

--- resources/views/vendor/filament/components/header/index.blade.php
+++ resources/views/vendor/filament/components/header/index.blade.php
@@ -7,7 +7,16 @@
 <header {{ $attributes->class(['filament-header space-y-2 items-start justify-between sm:flex sm:space-y-0 sm:space-x-4  sm:rtl:space-x-reverse sm:py-4']) }}>
     <div>
         <x-filament::header.heading>
-            {{ $heading }}
+            <div class="flex items-center">
+                <div>
+                    <img class="mr-2 w-10" src="/real-invest-logo.png" alt="logo">
+                </div>
+                <div>
+                    {{ $heading }}
+                </div>
+            </div>
+
+
         </x-filament::header.heading>
 
         @if ($subheading)
