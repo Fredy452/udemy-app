57e8559b7f6d15dea8a5d5fdfc0642e9966f869c
Fred452 <fredyarce452@gmail.com>
2023-03-18 14:33:47 -0300

    Creating compani name and publish vendor filament

---
+++ config/company.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'Real Invest',
+
+    'address' => [
+        'city' => 'California',
+        'street' => '555 Main St'
+    ]
+];
+
+?>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -137,7 +137,7 @@
             <span></span>
             <span></span>
         </button>
-        <a class="navbar-brand text-brand" href="index.html">Estate<span class="color-b">Agency</span></a>
+        <a class="navbar-brand text-brand" href="index.html">Real<span class="color-b"> Invest</span></a>
 
         <div class="navbar-collapse collapse justify-content-center" id="navbarDefault">
             <ul class="navbar-nav">
