95f7553c151d662ccac852da71615c9ff6d7dcbf
Fred452 <fredyarce452@gmail.com>
2023-03-24 13:55:36 -0300

    pruebas

---
+++ .history/resources/views/prueba.blade_20230322151916.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mi sitio web</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+</head>
+<body>
+	<header class="bg-gray-800 text-white py-4">
+		<div class="container mx-auto px-4">
+			<h1 class="text-2xl font-bold text-center">Mi sitio web</h1>
+		</div>
+	</header>
+
+	<nav class="bg-gray-200 py-2">
+		<div class="container mx-auto px-4">
+			<ul class="flex justify-center">
+				<li class="px-4">
+					<a href="#" class="text-gray-800 hover:text-gray-600">Inicio</a>
+				</li>
+				<li class="px-4">
+					<a href="#" class="text-gray-800 hover:text-gray-600">Acerca de</a>
+				</li>
+				<li class="px-4">
+					<a href="#" class="text-gray-800 hover:text-gray-600">Contacto</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+
+	<div class="bg-gray-400 h-64">
+		<img src="https://png.pngtree.com/thumb_back/fh260/back_pic/00/02/44/5056179b42b174f.jpg" alt="imagen">
+	</div>
+
+	<div class="container mx-auto py-8">
+		<h2 class="text-2xl font-bold mb-2">Últimas entradas</h2>
+
+		<div class="grid grid-cols-3 gap-4">
+			<div class="col-span-2">
+				<div class="bg-white p-4 rounded-lg shadow">
+					<h2 class="text-xl font-bold mb-2">Título de la entrada</h2>
+					<p class="text-gray-700 mb-4">Fecha de publicación: 1 de enero de 2023</p>
+					<p class="text-gray-700">Contenido de la entrada</p>
+					<a href="#" class="inline-block mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Leer más</a>
+				</div>
+
+				<div class="bg-white p-4 rounded-lg shadow mt-4">
+					<h2 class="text-xl font-bold mb-2">Título de la entrada</h2>
+					<p class="text-gray-700 mb-4">Fecha de publicación: 1 de enero de 2023</p>
+					<p class="text-gray-700">Contenido de la entrada</p>
+					<a href="#" class="inline-block mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Leer más</a>
+				</div>
+
+				<div class="bg-white p-4 rounded-lg shadow mt-4">
+					<h2 class="text-xl font-bold mb-2">Título de la entrada</h2>
+					<p class="text-gray-700 mb-4">Fecha de publicación: 1 de enero de 2023</p>
+					<p class="text-gray-700">Contenido de la entrada</p>
+					<a href="#" class="inline-block mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Leer más</a>
+				</div>
+			</div>
+
+			<div class="col-span-1">
+				<div class="bg-white p-4 rounded-lg shadow">
+					<h2 class="text-xl font-bold mb-2">Categorías</h2>
+					<ul>
+						<li><a href="#" class="text-gray-700 hover:text-gray-600">Categoría 1</a></li>
+						<li><a href="#" class="text-gray-700 hover:text-gray-600">Categoría 2</a></li>
+						<li><a href="#" class="text-gray-700 hover:text-gray-600">Categoría 3</a></li>
+					</ul>
+				</div>
+
+				<div class="col-span-2">
+					<div class="bg-white p-4 rounded-lg shadow">
+						<h2 class="text-xl font-bold mb-2">Etiquetas</h2>
+						<ul>
+							<li><a href="#" class="text-gray-700 hover:text-gray-600">Etiqueta 1</a></li>
+							<li><a href="#" class="text-gray-700 hover:text-gray-600">Etiqueta 2</a></li>
+							<li><a href="#" class="text-gray-700 hover:text-gray-600">Etiqueta 3</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
---
+++ .history/resources/views/prueba.blade_20230323125348.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Plantilla con Bootstrap</title>
+  </head>
+  <body>
+
+    <!-- Banner -->
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Bienvenido a mi sitio web</h1>
+        <p class="lead">Esta es una plantilla básica utilizando Bootstrap</p>
+      </div>
+    </div>
+
+    <!-- Barra de navegación -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Mi sitio web</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Acerca de</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Cuerpo de contenido -->
+    <div class="container">
+      <h2>Contenido principal</h2>
+      <p>Este es el contenido principal de mi sitio web.</p>
+    </div>
+
+    <!-- Pie de página -->
+    <footer class="bg-dark text-white">
+      <div class="container">
+        <p>Este es el pie de página de mi sitio web.</p>
+      </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
